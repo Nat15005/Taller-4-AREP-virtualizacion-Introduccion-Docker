1876374d97d0eaeff2a16d90f5d2d5279f4bc0e5
Nat <laura.rojas-r@mail.escuelaing.edu.co>
2025-02-18 15:49:01 -0500

    Funciona Todo

--- src/main/java/edu/escuelaing/arep/server/FileHandler.java
+++ src/main/java/edu/escuelaing/arep/server/FileHandler.java
@@ -20,22 +20,38 @@ public class FileHandler {
         if (resource.equals("/")) {
             resource = "/index.html"; // Redirigir a index.html si se accede a "/"
         }
-        Path filePath = Path.of(WebFramework.getStaticFolder() + resource);
-        if (!Files.exists(filePath)) {
-            sendNotFound(out);
-            return;
+
+        // Obtén la ruta base de los archivos estáticos
+        String staticFolder = WebFramework.getStaticFolder();
+        Path filePath = Path.of(staticFolder + resource);
+
+        if (Files.exists(filePath) && !Files.isDirectory(filePath)) {
+            // Si el archivo existe, envía los encabezados y el contenido
+            String contentType = getContentType(resource);
+            byte[] fileBytes = Files.readAllBytes(filePath);
+
+            // Construye los encabezados HTTP
+            String responseHeader = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: " + contentType + "\r\n" +
+                    "Access-Control-Allow-Origin: *\r\n" + // Permite solicitudes desde cualquier origen
+                    "Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\n" + // Métodos permitidos
+                    "Access-Control-Allow-Headers: Content-Type\r\n" + // Encabezados permitidos
+                    "Content-Length: " + fileBytes.length + "\r\n" +
+                    "\r\n";
+
+            // Envía los encabezados y el contenido del archivo
+            out.write(responseHeader.getBytes());
+            out.write(fileBytes);
+        } else {
+            // Si el archivo no existe, envía una respuesta 404
+            String response = "HTTP/1.1 404 Not Found\r\n" +
+                    "Access-Control-Allow-Origin: *\r\n" +
+                    "Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\n" +
+                    "Access-Control-Allow-Headers: Content-Type\r\n" +
+                    "\r\n" +
+                    "404 Not Found";
+            out.write(response.getBytes());
         }
-
-        String contentType = getContentType(resource);
-        byte[] fileBytes = Files.readAllBytes(filePath);
-
-        String responseHeader = "HTTP/1.1 200 OK\r\n" +
-                "Content-Type: " + contentType + "\r\n" +
-                "Content-Length: " + fileBytes.length + "\r\n" +
-                "\r\n";
-
-        out.write(responseHeader.getBytes());
-        out.write(fileBytes);
     }
 
     private static void sendNotFound(OutputStream out) throws IOException {
